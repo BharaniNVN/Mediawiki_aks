e57b0473bbd53c22ee6039c198cfe6a649d71d3f
Bharani NVN <bharini.naga@matrixcare.com>
2023-04-12 20:46:07 +0530

    Added files

--- Mediawiki_tf_files/terraform.tf
+++ Mediawiki_tf_files/terraform.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = "c4fdd105-c1e1-4e9b-86bc-322114e93639"
+  client_id       = ""
   client_secret   = var.sp
-  tenant_id       = "dffdd786-a9d0-43c2-ba4d-a11e3bef210b"
-  subscription_id = "1784f138-8764-4ad4-9b03-5374b8c4db22"
+  tenant_id       = ""
+  subscription_id = ""
 
   features {}
 }
--- README.md
+++ README.md
@@ -6,4 +6,3 @@ This repository describes, how to setup Mediawiki step by step in Azure Kubernet
 - Azure Account with an active subscription
 - Service principal account with contributor access on the subscription
 - Terraform installed machine or Azure Devops active project
-- AKS and ACR Configured in Azure cloud
